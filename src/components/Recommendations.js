91ed39338d62de792ba44c4a829e22b523ba6a0b
Saurav Sitaula <sitaulasaurav@gmail.com>
2026-01-20 01:21:46 +0545

    .

--- src/components/Recommendations.js
+++ src/components/Recommendations.js
@@ -853,7 +853,7 @@ export default function Recommendations() {
     : recommendations[activeTab].filter(item => item.genre === filter);
 
   return (
-    <section className="py-16 md:py-24 relative">
+    <section className="py-8 relative">
       <div className="max-w-7xl mx-auto px-4">
         {/* Section Header */}
         <div className="text-center mb-12">
@@ -975,7 +975,7 @@ export default function Recommendations() {
           {tabConfig.map((tab) => (
             <div 
               key={tab.id}
-              className={`neo-box p-4 text-center ${activeTab === tab.id ? tab.color + ' text-white' : 'bg-white'} transition-colors`}
+              className={`neo-box p-4 text-center ${activeTab === tab.id ? tab.color + ' ' : 'bg-white'} transition-colors`}
             >
               <span className="text-3xl block mb-2">{tab.emoji}</span>
               <span className="text-3xl font-black block">{recommendations[tab.id].length}</span>
